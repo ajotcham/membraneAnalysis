648327af76dc506903925665df76f2fda745fde9
Andrew Jotcham <ajotcham@andrews-mbp-8.wireless-1x.unc.edu>
2024-02-23 14:34:54 -0500

    flagged print messages and saves

--- evaluateMF.py
+++ evaluateMF.py
@@ -8,25 +8,27 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 import cProfile
 
-def profile(filename=None, comm=MPI.COMM_WORLD):
-  def prof_decorator(f):
-    def wrap_f(*args, **kwargs):
-      pr = cProfile.Profile()
-      pr.enable()
-      result = f(*args, **kwargs)
-      pr.disable()
 
-      if filename is None:
-        pr.print_stats()
-      else:
-        filename_r = filename + ".{}".format(comm.rank)
-        pr.dump_stats(filename_r)
 
-      return result
-    return wrap_f
-  return prof_decorator
+# def profile(filename=None, comm=MPI.COMM_WORLD):
+#   def prof_decorator(f):
+#     def wrap_f(*args, **kwargs):
+#       pr = cProfile.Profile()
+#       pr.enable()
+#       result = f(*args, **kwargs)
+#       pr.disable()
 
-@profile(filename="profile_out")
+#       if filename is None:
+#         pr.print_stats()
+#       else:
+#         filename_r = filename + ".{}".format(comm.rank)
+#         pr.dump_stats(filename_r)
+
+#       return result
+#     return wrap_f
+#   return prof_decorator
+
+# @profile(filename="profile_out")
 
 def my_function():
 
@@ -35,14 +37,16 @@ def my_function():
     rank = comm.Get_rank()
 
     subdomains = [2,2,2] # Specifies how Domain is broken among procs
-    nodes = [500,500,500] # Total Number of Nodes in Domain, controls resolution
+    ###increase number of nodes vs. MF values 
+    nodes = [100,100,100] # Total Number of Nodes in Domain, controls resolution
   
     ## Ordering for Inlet/Outlet ( (-x,+x) , (-y,+y) , (-z,+z) )
     boundaries = [[2,2],[2,2],[0,0]] # 0: Nothing Assumed  1: Walls 2: Periodic
     inlet  = [[0,0],[0,0],[0,0]]
     outlet = [[0,0],[0,0],[0,0]]
     
-    start_time = time.time()
+    if rank == 0:
+      start_time = time.time()
 
     r_lookup_file = './rLookups/PA.rLookup'
     r_lookup = pmmoto.io.read_r_lookup_file(r_lookup_file)
@@ -55,14 +59,22 @@ def my_function():
     domain_data[2] = [-75.329262, 75.329262]
 
     sd = pmmoto.initialize(rank,size,subdomains,nodes,boundaries,inlet,outlet)
-    pm = pmmoto.domain_generation.gen_pm_verlet_spheres(sd,sphere_data,domain_data,verlet=[8,8,8],add_periodic=True) 
+    pm = pmmoto.domain_generation.gen_pm_verlet_spheres(sd,sphere_data,domain_data,verlet=[12,12,12],add_periodic=True) 
     #Cpu time vs. # of verlet domains and change nodes if time available, iterate through verlet and node sizes
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print("elapsed time: ", elapsed_time)
+    mink = pmmoto.analysis.minkowski.functionals(sd,pm.grid)
 
-    pmmoto.io.save_grid_data("dataOut/test_lammps_read_grid_trim",sd,pm.grid)
+    if rank == 0:
+      print(mink)
+
+    if rank == 0:
+      end_time = time.time()
+      elapsed_time = end_time - start_time
+      print("elapsed time: ", elapsed_time)
+
+    save_grid = False
+    if save_grid:
+      pmmoto.io.save_grid_data("dataOut/test_lammps_read_grid_trim",sd,pm.grid)
 
 
 if __name__ == "__main__":
