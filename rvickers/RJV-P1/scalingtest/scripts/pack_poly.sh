a4d685dc3ecf2286a9ca4d8361268cd54ef110d0
Andrew Jotcham <ajotcham@Andrews-MacBook-Pro-13.local>
2024-10-10 14:59:58 -0400

    adjusted submit script and lammps input

--- rvickers/RJV-P1/scalingtest/scripts/pack_poly.sh
+++ rvickers/RJV-P1/scalingtest/scripts/pack_poly.sh
@@ -2,13 +2,11 @@
 #SBATCH --job-name=lammps_job
 #SBATCH --output=lammps_output.out
 #SBATCH --error=lammps_error.err
-#SBATCH --nodes=2
+#SBATCH --nodes=4
 #SBATCH --ntasks-per-node=32
 #SBATCH --time=12:00:00
 #SBATCH --partition=RM
 
-module load LAMMPS/3Mar20
-module load intel/20.4
-module load openmpi/3.1.6-gcc10.2.0
+module load intelmpi/2021.3.0-intel2021.3.0 gcc/10.2.0 cuda/11.7.1 LAMMPS/23Jun22-intel
 
-mpirun -np 64 lmp_mpi -in test_pack_polymerize.in -var mult 1 -var rand 1
+mpirun -np 128 lmp -in test_pack_polymerize.in -var mult 1 -var rand 1 -var xlink 0.93
